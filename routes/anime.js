6ed7c54c329da0f95e8f8ed9f47b4abd776f50aa
John Miguel Dungao <johnmiguel.dungao@gmail.com>
2024-04-07 21:15:28 -0400

    Refactor anime routes to use apicache middleware for caching and update cache durations

--- routes/anime.js
+++ routes/anime.js
@@ -4,7 +4,8 @@ const router = express.Router();
 const apicache = require("apicache");
 const baseURL = "https://zenkai-api.vercel.app/meta/anilist";
 let cache = apicache.middleware;
-router.get("/search/:q", async (req, res) => {
+
+router.get("/search/:q", cache("5 seconds"), async (req, res) => {
   try {
     const q = req.params.q;
     const response = await axios.get(`${baseURL}/${q}`);
@@ -32,20 +33,42 @@ router.get("/popular", cache("5 minutes"), async (req, res) => {
   }
 });
 
-router.get("/info/:id", cache("40 seconds"), async (req, res) => {
+router.get("/info/:id", cache("10 seconds"), async (req, res) => {
   const provider = ["gogoanime", "anify", "zoro"];
   try {
     const id = req.params.id;
     const promises = provider.map(async (p) => {
       try {
-        // ...
+        const response = await axios.get(`${baseURL}/info/${id}?provider=${p}`);
+        return response.data;
       } catch (error) {
-        // ...
+        throw error;
       }
     });
-    // ...
+
+    const response = await Promise.any(promises);
+    res.json(response);
   } catch (error) {
-    // ...
+    res.status(500).json({ error: error.message });
+  }
+});
+
+router.get("/watch/:id", cache("5 minutes"), async (req, res) => {
+  try {
+    const id = req.params.id;
+    const response = await axios.get(`${baseURL}/watch/${id}`);
+    res.json(response.data.sources);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+router.get("/airing-schedule", async (req, res) => {
+  try {
+    const response = await axios.get(`${baseURL}/airing-schedule`);
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
   }
 });
 
